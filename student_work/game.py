e11468321190e29b6aa6c03933a2de5fa26c3678
s979365 <s979365@student.knoxschools.org>
2026-02-26 16:59:06 +0000

    fixed dictionary

--- student_work/game.py
+++ student_work/game.py
@@ -1,27 +1,27 @@
 # Write your game here
 import curses
 game_data = { 
-'width':10,
-'height':10,
-{'truck': "\1F69A",
-'toilet': "\1F6BD",
-'rollercoaster': "\1F3A2"}
+    'width':10,
+    'height':10,
+    'game_elements': {'truck': "\U0001F69A",
+    'toilet': "\U0001F6BD",
+    'rollercoaster': "\U0001F3A2"},
     # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
 }
 #change this code to match where we want our icons
- stdscr.clear()
-    for y in range(game_data['height']):
-        row = ""
-        for x in range(game_data['width']):
-            # Truck icon
-            if x == game_data['truck'][''] and game_data['truck']['']:
-            # Toilet icon
-            if x == game_data['toilet'][''] and game_data['toilet']['']:
-            # Roller coaster icon
-            if x == game_data['rollercoaster'][''] and game_data['rollercoaster']['']:
-            else:
-                row += game_data['empty']
-        stdscr.addstr(y, 0, row, curses.color_pair(1))
+#  stdscr.clear()
+    # for y in range(game_data['height']):
+    #     row = ""
+    #     for x in range(game_data['width']):
+    #         # Truck icon
+    #         if x == game_data['truck'][''] and game_data['truck']['']:
+    #         # Toilet icon
+    #         if x == game_data['toilet'][''] and game_data['toilet']['']:
+    #         # Roller coaster icon
+    #         if x == game_data['rollercoaster'][''] and game_data['rollercoaster']['']:
+    #         else:
+    #             row += game_data['empty']
+    #     stdscr.addstr(y, 0, row, curses.color_pair(1))
 
  
     # Print the board and all game elements using curses
@@ -32,6 +32,8 @@ def draw_board(screen):
     screen.refresh()
     screen.getkey()  # pause so player can see board
 
-curses.wrapper(draw_board)
+# curses.wrapper(draw_board)
+
+print(game_data['game_elements']['toilet'])
 
 # Good Luck!
