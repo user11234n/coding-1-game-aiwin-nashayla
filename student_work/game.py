4c7af010e71bf8f0a57b9b8f0887ec3f14615108
s979365 <s979365@student.knoxschools.org>
2026-02-26 16:27:04 +0000

    icon positions

--- student_work/game.py
+++ student_work/game.py
@@ -8,6 +8,17 @@ game_data = {
 'rollercoaster': "\1F3A2"}
     # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
 }
+#change this code to match where we want our icons
+ stdscr.clear()
+    for y in range(game_data['height']):
+        row = ""
+        for x in range(game_data['width']):
+            # Player
+            if x == game_data['player']['x'] and y == game_data['player']['y']:
+                row += game_data['turtle']
+            else:
+                row += game_data['empty']
+        stdscr.addstr(y, 0, row, curses.color_pair(1))
 
  
     # Print the board and all game elements using curses
