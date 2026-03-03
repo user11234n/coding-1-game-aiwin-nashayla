3035f09597f6ab5e0ad271c74755c60587ea8e30
s979365 <s979365@student.knoxschools.org>
2026-03-03 16:47:36 +0000

    demon kitty

--- student_work/game.py
+++ student_work/game.py
@@ -6,11 +6,17 @@ game_data = {
     'player': {"x": 0, "y": 0},
     'game_elements': {'truck': "\U0001F69A",
     'toilet': "\U0001F6BD",
-    'rollercoaster': "\U0001F3A2"},
+    'rollercoaster': "\U0001F3A2",
+    'demon kitty': "\U0001F638"},
     # Store board dimensions, player/enemy positions, score, energy, collectibles, and icons
 }
 
     # Print the board and all game elements using curses
+for y in range(game_data['height']):
+    row = ""
+    for x in range(game_data['width']):
+        if x == game_data['player']['x'] and y == game_data['player']['y']:
+            row += game_data['game_elements']['demon kitty']
 def draw_board(stdscr):
     curses.start_color()
     curses.use_default_colors()
@@ -24,6 +30,12 @@ def move_player(key):
     y = game_data['player']['y']
     new_x, new_y = x, y
     key = key.lower()
+    if key == "w" and y > 0:
+        new_y -= 1
+    elif key == "s" and y < game_data['height'] - 1:
+        new_y += 1    
+    else:
+        return
 # curses.wrapper(draw_board)
 
 print(game_data['game_elements']['toilet'])
